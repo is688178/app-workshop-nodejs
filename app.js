6314b9f004fbe3ed8c7c1b7aed31f23f6dca471a
is688178 <is688178@gmail.com>
2021-11-28 15:52:50 -0600

    Change to http

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('login.html');
 const express = require("express");
 const admin = require("firebase-admin");
 const cookieParser = require("cookie-parser");
-const https = require('https');
-const fs = require('fs');
 require('dotenv').config()
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const app = express();
-const port = process.env.port || 3000;
 app.use(cookieParser());
 
 var key = process.env.PRIVATE_KEY;
@@ -22,7 +23,6 @@ admin.initializeApp({
 });
 
 app.get('/', (req, res) => {
-
     res.sendFile(__dirname + '/login.html');  //You can use render in case of ejs 
 });
 
@@ -31,80 +31,57 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-app.get('/success', checkCookie, (req, res) => {
+app.get('/success', (req, res) => {
     res.sendFile(__dirname + '/success.html');
-    console.log("UID of Signed in User is: " + req.decodedClaims.uid);
-    //You will reach here only if session is working Fine
 });
 
-app.get('/spare_parts', checkCookie, (req, res) => {
+app.get('/spare_parts', (req, res) => {
     res.sendFile(__dirname + '/spare_parts.html');
-    console.log("UID of Signed in User is: " + req.decodedClaims.uid);
-    //You will reach here only if session is working Fine
 });
 
-app.get('/create_cone', checkCookie, (req, res) => {
+app.get('/create_cone', (req, res) => {
     res.sendFile(__dirname + '/create_cone.html');
-    console.log("UID of Signed in User is: " + req.decodedClaims.uid);
-    //You will reach here only if session is working Fine
 });
 
-app.get('/create_sp', checkCookie, (req, res) => {
+app.get('/create_sp', (req, res) => {
     res.sendFile(__dirname + '/create_sp.html');
-    console.log("UID of Signed in User is: " + req.decodedClaims.uid);
-    //You will reach here only if session is working Fine
 });
 
-app.get('/savecookie', (req, res) => {
-    const Idtoken = req.query.idToken;
-    console.log("Idtoken of Signed in User is: " + Idtoken);
-    savecookie(Idtoken, res);
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
 
-//saving cookies and verify cookies
-// Reference : https://firebase.google.com/docs/auth/admin/manage-cookies
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
 
-function savecookie(idtoken, res) {
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
 
-    const expiresIn = 60 * 60 * 24 * 5 * 1000;
-    admin.auth().createSessionCookie(idtoken, { expiresIn })
-        .then((sessionCookie) => {
-            const options = { maxAge: expiresIn, httpOnly: true, secure: true };
-            res.cookie('__session', sessionCookie, options);
+module.exports = app;
 
-            admin.auth().verifyIdToken(idtoken).then(function (decodedClaims) {
-                res.redirect('/success');
-            });
-
-        }, error => {
-            console.log(error);
-            res.status(401).send("UnAuthorised Request");
-
-        });
-}
-
-
-function checkCookie(req, res, next) {
-
-
-    const sessionCookie = req.cookies.__session || '';
-    admin.auth().verifySessionCookie(
-        sessionCookie, true).then((decodedClaims) => {
-            req.decodedClaims = decodedClaims;
-            next();
-        })
-        .catch(error => {
-            // Session cookie is unavailable or invalid. Force user to login.
-            res.redirect('/login');
-        });
-
-}
-
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app)
-    .listen(port, function () {
-        console.log('Simple workshop running on port 3000! Go to https://localhost:3000/')
-    });
 
