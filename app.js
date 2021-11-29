18449bb952d5b2018626d38bb14f0c8d510f9462
is688178 <is688178@gmail.com>
2021-11-28 18:25:55 -0600

    HTTP Only with headers url directions

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const express = require("express");
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('success.html');
 const admin = require("firebase-admin");
-const cookieParser = require("cookie-parser");
-const http = require('http');
-const fs = require('fs');
 require('dotenv').config()
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var port = process.env.port || 3000;
-
-const app = express();
 
 var key = process.env.PRIVATE_KEY;
 
@@ -20,33 +17,62 @@ admin.initializeApp({
     })
 });
 
-app.get('/', (req, res) => {
+var log = function (entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-    res.sendFile(__dirname + '/success.html');
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function () {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+            res.end();
+        });
+    } else {
+
+        if (req.url === '/spare_parts') {
+            html = fs.readFileSync('spare_parts.html');
+            res.writeHead(200);
+            res.write(html);
+            res.end();
+        } else if (req.url === '/create_cone') {
+            html = fs.readFileSync('create_cone.html');
+            res.writeHead(200);
+            res.write(html);
+            res.end();
+        } else if (req.url === '/create_sp') {
+            html = fs.readFileSync('create_sp.html');
+            res.writeHead(200);
+            res.write(html);
+            res.end();
+        } else if (req.url === '/success'){
+            html = fs.readFileSync('success.html');
+            res.writeHead(200);
+            res.write(html);
+            res.end();
+        } else if (req.url === '/'){
+            html = fs.readFileSync('success.html');
+            res.writeHead(200);
+            res.write(html);
+            res.end();
+        }
+
+    }
 });
 
-app.get('/logout', (req, res) => {
-    res.redirect('/');
-});
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
 
-app.get('/success', (req, res) => {
-    res.sendFile(__dirname + '/success.html');
-});
-
-app.get('/spare_parts', (req, res) => {
-    res.sendFile(__dirname + '/spare_parts.html');
-});
-
-app.get('/create_cone', (req, res) => {
-    res.sendFile(__dirname + '/create_cone.html');
-});
-
-app.get('/create_sp', (req, res) => {
-    res.sendFile(__dirname + '/create_sp.html');
-});
-
-http.createServer(app)
-    .listen(port, function () {
-        console.log('Simple workshop running on port 3000! Go to http://localhost:3000/')
-    });
-
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
